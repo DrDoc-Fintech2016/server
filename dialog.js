c132be1e51c38245e74d874fd6f8e0eadc964705
czarfati <chenzarf@gmail.com>
2016-11-16 20:47:50 +0200

    added paycheck model json

--- dialog.js
+++ dialog.js
@@ -2,7 +2,7 @@
  * Created by czarfati on 11/16/2016.
  */
 var apiai = require('apiai');
-var hardcode_access_token = null;
+var hardcoded_access_token = 'c421d45054224c8180fe4c4d9f42335e';
 
 function ApiAiHandler(access_token) {
     var app = apiai(access_token);
@@ -20,7 +20,7 @@ function ApiAiHandler(access_token) {
 }
 
 function DialogManager(){
-    this.apiai_handler = ApiAiHandler(hardcode_access_token);
+    this.apiai_handler = ApiAiHandler(hardcoded_access_token);
 
     var process_question = function(){
 
